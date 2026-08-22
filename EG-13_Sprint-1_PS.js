bbb12b2834faa0515e762fc378aacf60c6428bf0
saikot05 <saikotislam08@gmail.com>
2026-08-22 09:25:06 +0600

    solve Fibonnacci problem

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -6,3 +6,19 @@
 function isLeapYear(year) {
     return ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0);
 }
+
+//02. Generate Fibonacci Sequence Up to N Terms/**
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+function generateFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+
+    const fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
